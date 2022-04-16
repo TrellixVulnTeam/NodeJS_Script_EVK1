dbcd88dfd41a1244ef097374356dde54ccb80086
Akshay Raj <akshay.contrive@gmail.com>
2022-04-16 11:26:52 +0530

    complaints script

--- controller/Controller.js
+++ controller/Controller.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const fs = require('fs')
 const XLSX = require('node-xlsx');
 const axios = require('axios').default;
+const FormData = require('form-data');
 
 const submitFeedback = async (req, res, next) => {
   const id = req.params.id;
@@ -82,6 +83,48 @@ const submitRegister = async (req, res, next) => {
   res.status(200).json({})
 }
 
+const submitComplaint = async (req, res, next) => {
+  const fromId = req.query.from ?? 0;
+  const file = `${__dirname}/../public/complaints.xlsx`;
+
+  // Logs
+  const log_path = `${__dirname}/../public/log_complaints_${Date.now()}.txt`;
+  const myConsole = new console.Console(fs.createWriteStream(log_path));
+
+  try {
+    if (!fs.existsSync(file)) return next(createError({ message: 'File not found' }))
+  } catch (err) {
+    console.error(err)
+  }
+
+  const sheet = XLSX.parse(fs.readFileSync(file));
+
+  const sheetData = sheet[0].data;
+  const newSheet = [['mobile', 'complaintId', 'latitude', 'longitude', 'status']]
+
+  for (var x = 1; x < sheetData.length; x++) {
+    const data = sheetData[x];
+    const randomFile = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    const photo = `${__dirname}/../public/images/before/${randomFile}.jpg`;
+
+    var response = await complaintData(data, photo);
+    newSheet.push([data[1], response.complaint.comId, data[4], data[5], response.message])
+
+    let buffer = XLSX.build([{ name: 'Sheet', data: newSheet }]);
+    fs.writeFileSync(`${__dirname}/../public/complete_complaints.xlsx`, buffer);
+
+    myConsole.log(`${x} | ${data[1]} | ${response.complaint.comId}`);
+
+    setInterval(() => { }, 5000);
+
+    if (x == sheetData.length - 1) {
+      console.log('Complaints Completed!');
+    }
+  }
+
+  res.status(200).json({})
+}
+
 /**
  * 
  * @param {mobile} mobile 
@@ -118,7 +161,32 @@ const feedbackData = async (mobile, age) => {
     });
 }
 
+const complaintData = async (data, photo) => {
+  console.log(data);
+
+  const form = new FormData();
+  form.append('categoryId', data[0]);
+  form.append('mobile', data[1]);
+  form.append('name', data[2]);
+  form.append('complaint', data[3]);
+  form.append('latitude', data[4]);
+  form.append('longitude', data[5]);
+  form.append('address', data[6]);
+  form.append('landmark', data[7]);
+  form.append('photo', fs.createReadStream(photo));
+
+  return axios.post(`http://97.74.85.82/bhopal-first/complaint`, form, { headers: form.getHeaders() })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error.response);
+      return error.response;
+    });
+}
+
 module.exports = {
   submitFeedback: submitFeedback,
-  submitRegister: submitRegister
+  submitRegister: submitRegister,
+  submitComplaint: submitComplaint
 }
--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function (req, res, next) {
 
 router.get('/feedback2/:id', Controller.submitFeedback)
 router.get('/register2/:id', Controller.submitRegister)
+router.get('/complaints', Controller.submitComplaint)
 
 router.get('/register', async (req, res, next) => {
   const file = `${__dirname}/../public/register.xlsx`;
